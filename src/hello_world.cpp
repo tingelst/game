2b6f4e35b2257042fb18248c1bbff64410edde03
Lars Tingelstad <lars.tingelstad@ntnu.no>
2015-09-23 09:14:59 +0200

    Small fixes

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -4,6 +4,8 @@
 #include <boost/python.hpp>
 #include <ceres/ceres.h>
 
+#include "game/ceres_python_utils.h"
+
 namespace bp = boost::python;
 namespace np = boost::numpy;
 
@@ -12,6 +14,9 @@ using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solver;
 using ceres::Solve;
+
+namespace hello_world
+{
 
 struct OptimizationProblem {
   OptimizationProblem(const OptimizationProblem& optimization_problem){};
@@ -35,39 +40,16 @@ struct OptimizationProblem {
     Solve(options_, &problem_, &summary_);
   }
 
-  auto Summary() -> bp::dict {
-    bp::dict summary;
-    summary["linear_solver_type_used"] = std::string(
-        ceres::LinearSolverTypeToString(summary_.linear_solver_type_used));
-    summary["num_parameter_blocks"] = summary_.num_parameter_blocks;
-    summary["num_parameters"] = summary_.num_parameters;
-    summary["num_residual_blocks"] = summary_.num_residual_blocks;
-    summary["num_residuals"] = summary_.num_residuals;
-
-    bp::list iterations;
-    auto its = summary_.iterations;
-    for (int i = 0; i < its.size(); ++i) {
-      bp::dict iteration;
-      iteration["iteration"] = its[i].iteration;
-      iteration["cost"] = its[i].cost;
-      iteration["cost_change"] = its[i].cost_change;
-      iteration["gradient_max_norm"] = its[i].gradient_max_norm;
-      iteration["step_norm"] = its[i].step_norm;
-      iteration["relative_decrease"] = its[i].relative_decrease;
-      iteration["trust_region_radius"] = its[i].trust_region_radius;
-      iteration["eta"] = its[i].eta;
-      iteration["linear_solver_iterations"] = its[i].linear_solver_iterations;
-      iterations.append(iteration);
-    }
-    summary["iterations"] = iterations;
-    summary["brief_report"] = summary_.BriefReport();
-    return summary;
-  }
+  auto Summary() -> bp::dict { return game::SummaryToDict(summary_); }
 
   Problem problem_;
   Solver::Options options_;
   Solver::Summary summary_;
 };
+
+}
+
+using namespace hello_world;
 
 BOOST_PYTHON_MODULE(libhello_world) {
   np::initialize();

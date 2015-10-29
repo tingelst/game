d2c7679bf13a8dda6c5713a15a131ef5f7a28e92
Lars Tingelstad <lars.tingelstad@ntnu.no>
2015-10-29 08:09:11 +0100

    kinematic calibration slow jacobian

---
+++ include/game/robot_kinematics.h
@@ -0,0 +1,15 @@
+//
+// Created by lars on 26.10.15.
+//
+
+#ifndef GAME_ROBOT_KINEMATICS_H
+#define GAME_ROBOT_KINEMATICS_H
+
+
+class robot_kinematics
+{
+
+};
+
+
+#endif //GAME_ROBOT_KINEMATICS_H
---
+++ src/kinematic_calibration_wrapper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lars on 26.10.15.
+//
+
+#include "kinematic_calibration_wrapper.h"

d38055b60db5bcb18b59ef2166d8b1f1d287cfe1
Lars Tingelstad <lars.tingelstad@ntnu.no>
2015-12-03 12:17:58 +0100

    lds_calibration works, need to check in pose generator as it is unstable.

--- src/lds_calibration.cpp
+++ src/lds_calibration.cpp
@@ -86,6 +86,55 @@ class LaserDistanceSensorCalibrator {
     SetSolverOptions(solver_options, options_);
   }
 
+  LaserDistanceSensorCalibrator(const Vec& point, const double theta,
+                                const double phi,
+                                const bp::dict& solver_options)
+      : point_(point), theta_(theta), phi_(phi) {
+    SetSolverOptions(solver_options, options_);
+  }
+
+  struct AnglesDistanceErrorCostFunctor {
+    AnglesDistanceErrorCostFunctor(const Mot& forward_kinematics,
+                                   const Dlp& measurement_plane,
+                                   const double measurement_distance)
+        : forward_kinematics_(forward_kinematics),
+          measurement_plane_(measurement_plane),
+          measurement_distance_(measurement_distance) {}
+
+    template <typename T>
+    auto operator()(const T* const point, const T* const theta,
+                    const T* const phi, T* residual) const -> bool {
+      Motor<T> M_forward_kinematics(forward_kinematics_);
+      DualPlane<T> dlp(measurement_plane_);
+
+      Vector<T> l0 = Vector<T>(point).null().spin(M_forward_kinematics);
+
+      Vector<T> direction = Vector<T>(T(1.0), T(0.0), T(0.0));
+      direction = direction.spin(Rotor<T>(
+          cos(theta[0] / T(2.0)), sin(theta[0] / T(2.0)), T(0.0), T(0.0)));
+      // direction = direction.spin(
+      // Rotor<T>(cos(phi[0] / T(2.0)), T(0.0), T(0.0), sin(phi[0] / T(2.0))));
+
+      auto l = (Vector<T>(direction).unit() ^ Infinity<T>(T(1.0)))
+                   .spin(M_forward_kinematics);
+
+      Vector<T> n(dlp[0], dlp[1], dlp[2]);
+      Vector<T> p0(dlp[0] * dlp[3], dlp[1] * dlp[3], dlp[2] * dlp[3]);
+      Vector<T> p0l0 = p0 - l0;
+      T num = ((p0 - l0) <= n)[0];
+      T den = (Vector<T>(l[0], l[1], l[2]) <= n)[0];
+      T distance = num / den;
+      residual[0] = T(measurement_distance_) - distance;
+
+      return true;
+    }
+
+   private:
+    const Mot forward_kinematics_;
+    const Dlp measurement_plane_;
+    const double measurement_distance_;
+  };
+
   struct PointDirectionDistanceErrorCostFunctor {
     PointDirectionDistanceErrorCostFunctor(const Mot& forward_kinematics,
                                            const Dlp& measurement_plane,
@@ -167,6 +216,23 @@ class LaserDistanceSensorCalibrator {
     const double measurement_distance_;
   };
 
+  struct DirectionNormalizationParameterization {
+    template <typename T>
+    bool operator()(const T* x, const T* delta, T* x_plus_delta) const {
+      std::cout << "Direction Normalization" << std::endl;
+      x_plus_delta[0] = x[0] + delta[0];
+      x_plus_delta[1] = x[1] + delta[1];
+      x_plus_delta[2] = x[2] + delta[2];
+      T scale = static_cast<T>(1.0) / sqrt(x_plus_delta[0] * x_plus_delta[0] +
+                                           x_plus_delta[1] * x_plus_delta[1] +
+                                           x_plus_delta[2] * x_plus_delta[2]);
+      x_plus_delta[0] *= scale;
+      x_plus_delta[1] *= scale;
+      x_plus_delta[2] *= scale;
+      return true;
+    }
+  };
+
   auto Summary() const -> bp::dict { return game::SummaryToDict(summary_); }
 
   auto AddTangentVectorResidualBlock(const Mot& forward_kinematics,
@@ -192,6 +258,18 @@ class LaserDistanceSensorCalibrator {
             new PointDirectionDistanceErrorCostFunctor(
                 forward_kinematics, measurement_plane, measurement_distance));
     problem_.AddResidualBlock(cost_function, NULL, &point_[0], &direction_[0]);
+    return true;
+  }
+
+  auto AddAngleDistanceResidualBlock(const Mot& forward_kinematics,
+                                     const Dlp& measurement_plane,
+                                     const double measurement_distance)
+      -> bool {
+    ceres::CostFunction* cost_function =
+        new ceres::AutoDiffCostFunction<AnglesDistanceErrorCostFunctor, 1, 3, 1,
+                                        1>(new AnglesDistanceErrorCostFunctor(
+            forward_kinematics, measurement_plane, measurement_distance));
+    problem_.AddResidualBlock(cost_function, NULL, &point_[0], &theta_, &phi_);
     return true;
   }
 
@@ -225,6 +303,16 @@ class LaserDistanceSensorCalibrator {
     }
   }
 
+  auto SetDirectionParameterizationTypeFromString(const std::string& type)
+      -> bool {
+    if (type == "NORMALIZE") {
+      problem_.SetParameterization(
+          &direction_[0], new ceres::AutoDiffLocalParameterization<
+                              DirectionNormalizationParameterization, 3, 3>);
+      return true;
+    }
+  }
+
   auto Solve() -> bp::tuple {
     ceres::Solve(options_, &problem_, &summary_);
     return bp::make_tuple(motor_, Summary());
@@ -235,10 +323,17 @@ class LaserDistanceSensorCalibrator {
     return bp::make_tuple(point_, direction_, Summary());
   }
 
+  auto SolvePointAngles() -> bp::tuple {
+    ceres::Solve(options_, &problem_, &summary_);
+    return bp::make_tuple(point_, theta_, phi_, Summary());
+  }
+
  private:
   Mot motor_;
   Vec point_;
   Vec direction_;
+  double theta_;
+  double phi_;
 
   ceres::Problem problem_;
   ceres::Solver::Options options_;
@@ -249,13 +344,21 @@ BOOST_PYTHON_MODULE_INIT(liblds_calibration) {
   bp::class_<LaserDistanceSensorCalibrator>(
       "LaserDistanceSensorCalibrator", bp::init<const Mot&, const bp::dict&>())
       .def(bp::init<const Vec&, const Vec&, const bp::dict&>())
+      .def(bp::init<const Vec&, const double, const double, const bp::dict&>())
       .def("add_tangent_vector_residual_block",
            &LaserDistanceSensorCalibrator::AddTangentVectorResidualBlock)
       .def("add_point_direction_residual_block",
            &LaserDistanceSensorCalibrator::AddPointDirectionResidualBlock)
+      .def("add_point_angles_residual_block",
+           &LaserDistanceSensorCalibrator::AddAngleDistanceResidualBlock)
       .def("set_parameterization", &LaserDistanceSensorCalibrator::
                                        SetMotorParameterizationTypeFromString)
+      .def("set_direction_parameterization",
+           &LaserDistanceSensorCalibrator::
+               SetDirectionParameterizationTypeFromString)
       .def("solve", &LaserDistanceSensorCalibrator::Solve)
+      .def("solve_point_angles",
+           &LaserDistanceSensorCalibrator::SolvePointAngles)
       .def("solve_point_direction",
            &LaserDistanceSensorCalibrator::SolvePointDirection);
 

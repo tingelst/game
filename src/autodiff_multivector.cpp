fce388aeeeb289407e305b27c9983b7449f0fc82
Lars Tingelstad <lars.tingelstad@ntnu.no>
2016-03-14 20:23:13 +0100

    testing directional derivative

--- src/autodiff_multivector.cpp
+++ src/autodiff_multivector.cpp
@@ -34,6 +34,14 @@ void InnerProductVectorBivector(const T *vec, const T *biv, T *res) {
 }
 
 template <typename T>
+void InnerProductVectorVector(const T *vec, const T *biv, T *res) {
+  Vector<T> vector(vec);
+  Vector<T> vector(vec2);
+  Scalar<T> result = vector <= bivector;
+  res[0] = result[0];
+}
+
+template <typename T>
 void InnerProductVectorBivector2(const T *a, const T *b, T *res) {
   res[0] = -a[2] * b[1] - a[1] * b[0];
   res[1] = -a[2] * b[2] + a[0] * b[0];
@@ -97,6 +105,39 @@ struct InnerProductVectorBivectorFunctor {
   }
 };
 
+struct InnerProductVectorVectorFunctor {
+  template <typename T>
+  bool operator()(const T *vec, const T *biv, T *res) const {
+    InnerProductVectorVector(vec, biv, res);
+    return true;
+  }
+};
+
+py::list CeresDiffInnerProductVectorVector(const Vec &vec, const Vec &vec2) {
+  auto py_array_jac = py::array(py::buffer_info(
+      nullptr, sizeof(double), py::format_descriptor<double>::value(), 2,
+      {3, 3}, {sizeof(double) * 3, sizeof(double)}));
+
+  auto py_array_result = py::array(py::buffer_info(
+      nullptr, sizeof(double), py::format_descriptor<double>::value(), 2,
+      {1, 1}, {sizeof(double), sizeof(double)}));
+
+  auto buf_jac = py_array_jac.request();
+  auto buf_res = py_array_result.request();
+
+  const double *parameters[2] = {vec.begin(), vec2.begin()};
+  double *jacobians[2] = {static_cast<double *>(buf_jac.ptr), nullptr};
+
+  ceres::AutoDiffCostFunction<InnerProductVectorVectorFunctor, 1, 3, 3>(
+      new InnerProductVectorVectorFunctor())
+      .Evaluate(parameters, static_cast<double *>(buf_res.ptr), jacobians);
+
+  py::list list;
+  list.append(py_array_result);
+  list.append(py_array_jac);
+
+  return list;
+}
 py::list CeresDiffInnerProductVectorBivector(const Vec &vec, const Biv &biv) {
   auto py_array_jac = py::array(py::buffer_info(
       nullptr, sizeof(double), py::format_descriptor<double>::value(), 2,
@@ -146,13 +187,11 @@ py::list StanDiffInnerProductVectorBivector(const Vec &vec, const Biv &biv) {
   for (int i = 0; i < 3; ++i) res[i] = vec_ip_biv[i].val();
 
   auto jac = static_cast<double *>(buf_jac.ptr);
-  Matrix<double, 3, 3> J;
   for (int i = 0; i < 3; ++i) {
     if (i > 0) stan::math::set_zero_all_adjoints();
     vec_ip_biv[i].grad();
     for (int j = 0; j < 3; ++j) jac[3 * i + j] = vector[j].adj();
   }
-
 
   py::list list;
   list.append(py_array_result);
@@ -165,6 +204,7 @@ PYBIND11_PLUGIN(autodiff_multivector) {
   py::module m("autodiff_multivector", "autodiff_multivector");
   m.def("diff_adept", &AdeptDiffInnerProductVectorBivector);
   m.def("diff_ceres", &CeresDiffInnerProductVectorBivector);
+  m.def("diff_ceres2", &CeresDiffInnerProductVectorVector);
   m.def("diff_stan", &StanDiffInnerProductVectorBivector);
 
   return m.ptr();

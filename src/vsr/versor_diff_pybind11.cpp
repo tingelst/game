24b11eae65ff9cb9c3b9582c7bf508be09e186fc
Lars Tingelstad <lars.tingelstad@ntnu.no>
2016-03-29 10:08:48 +0200

    Added versor_diff_pybind11.cpp

---
+++ src/vsr/versor_diff_pybind11.cpp
@@ -0,0 +1,30 @@
+#include <pybind11/pybind11.h>
+
+#include "game/vsr/cga_op.h"
+#include "game/vsr/cga_types.h"
+
+namespace vsr {
+
+namespace python {
+
+namespace py = pybind11;
+using namespace vsr::cga;
+
+void AddMultivectorDiff(py::module &m) {
+
+  m.def("valkenburg", [](const Dll &a, const Dll &b) {
+    auto two_b = b * 2.0;
+    auto bb_inv = !(b * b);
+    auto ret =
+        Mot(a) +
+        (Sca(1.0) - two_b - Gen::motor(-two_b)) * bb_inv * Dll(b * a) * 0.5;
+    std::cout << ret << std::endl;
+    return ret;
+  });
+
+  m.def("diff_rotor", [](const Rot &rot, const Biv &biv, const Vec &vec) {
+    return (biv * Gen::log(rot) * rot * vec * ~rot)[0];
+  });
+}
+}
+}
